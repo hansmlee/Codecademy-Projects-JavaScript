55ed0385da8967f5ec58b5b922614a273f2bb6b7
Meng Han Lee <57082904+hansmlee@users.noreply.github.com>
2021-08-11 16:31:41 +0800

    Create secret-message.js

---
+++ 06-Secret-Message/secret-message.js
@@ -0,0 +1,14 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+secretMessage.pop();
+//console.log(secretMessage);
+console.log(secretMessage.length);
+secretMessage.push('to', 'Program');
+//console.log(secretMessage);
+secretMessage[7] = 'right';
+//console.log(secretMessage);
+secretMessage.shift();
+//console.log(secretMessage);
+secretMessage.unshift('Programming');
+//console.log(secretMessage);
+secretMessage.splice('get', 'right', 'the', 'first', 'time');
+console.log(secretMessage);
