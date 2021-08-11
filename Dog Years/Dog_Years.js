b86e6964b6140fbe0af2298786338e05cd8e2d9f
Meng Han Lee <57082904+hansmlee@users.noreply.github.com>
2021-08-11 15:12:10 +0800

    Create Dog_Years.js

---
+++ Dog Years/Dog_Years.js
@@ -0,0 +1,15 @@
+//Create a variable and set it equal to the age
+let myAge = 29;
+
+//Create a variable named earlyYears
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+//Add earlyYears and laterYears together
+let myAgeInDogYears = earlyYears + laterYears;
+myName = 'Meng Han Lee'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
